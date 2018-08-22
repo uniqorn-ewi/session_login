0e0264cdbc5259f5fc362bb23bc13953ed10519b
Yuji YAMAMOTO <oo_hayjp@yahoo.co.jp>
2018-08-23 03:16:53 +0900

    ph4 高度な機能実践3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,30 @@
 class User < ApplicationRecord
-  validates :name,  presence: true, length: { maximum: 30 }
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
-                    uniqueness: { case_sensitive: :false }
+  validates :name, presence: true, length: { maximum: 30 }
+  validates(
+    :email,
+    presence: true,
+    length: { maximum: 255 },
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+    uniqueness: { case_sensitive: false }
+  )
   before_save { email.downcase! }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
-
   has_many :blogs
+
+  has_many(
+    :active_relationships,
+    foreign_key: 'follower_id',
+    class_name: 'Relationship',
+    dependent: :destroy
+  )
+  has_many :following, through: :active_relationships, source: :followed
+
+  has_many(
+    :passive_relationships,
+    foreign_key: 'followed_id',
+    class_name: 'Relationship',
+    dependent: :destroy
+  )
+  has_many :followers, through: :passive_relationships, source: :follower
 end
